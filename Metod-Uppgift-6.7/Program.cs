137b60592c03b4a874cde8304129ffb439a0a53c
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-19 22:35:40 +0100

    Add project files.

---
+++ Metod-Uppgift-6.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+namespace Uppgift_6_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Skri in ett tal som är större än ett för att undersöka om det är ett primtal");
+            Console.WriteLine(Prim(int.Parse(Console.ReadLine())));
+        }
+
+        static bool Prim(int tal)
+        {
+            for (int i = 2; i < tal - 1; i++)
+            {
+                if (tal % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
